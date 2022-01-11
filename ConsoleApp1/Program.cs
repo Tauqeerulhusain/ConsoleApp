aabf5e4141584d885b4fa3948b0796190b995b58
tauqueerulhusain <tauqueerulhusain@gmail.com>
2022-01-11 09:34:51 +0530

    initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using ConsoleApp1.Models;
+using System;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = { 1, 2, 0 };
+            var sortedNum = num.OrderBy(x => x).ToArray();
+            var firstElement = sortedNum[0];
+            foreach (var element in sortedNum)
+            {
+                if(!sortedNum.Contains(firstElement))
+                {
+
+                }
+                else
+                {
+
+                }
+                firstElement++;
+
+            }
+        }
+    }
+}
